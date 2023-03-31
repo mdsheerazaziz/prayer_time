3bb4c3a552421b81f1fec22bc8ccb853c84cf631
Mohd <mohd.sheeraz2011@vit.ac.in>
2023-03-31 21:12:29 +0530

    Fixed config and requirements

--- launcher.sh
+++ launcher.sh
@@ -5,6 +5,6 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
-sudo cp supervisor_conf.conf ~/etc/supervisor/conf.d/azan_app.conf
+sudo cp supervisor_conf.conf /etc/supervisor/conf.d/azan_app.conf
 sudo supervisorctl reread
 sudo service supervisor restart
