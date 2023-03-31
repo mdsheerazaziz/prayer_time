d7c9c7cb68e222267d7b250b4909a052b6f81f4e
Mohd <mohd.sheeraz2011@vit.ac.in>
2023-03-31 20:59:13 +0530

    Added Config

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 # launcher.sh
+sudo apt-get install nginx supervisor
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+pip install gunicorn
+sudo cp supervisor_conf.conf ~/etc/supervisor/conf.d/azan_app.conf
+sudo supervisorctl reread
+sudo service supervisor restart
